698855d76001867268d32c6262972167713d1384
Mayur <mayur.jaganmohan@gmail.com>
2020-04-10 20:11:33 +0530

    Section 9 - Fully working - CREATE, UPDATE, DELETE, GET Categories

--- controllers/category.js
+++ controllers/category.js
@@ -12,3 +12,57 @@ exports.getCategoryById = (req, res, next, id) => {
     next();
   });
 };
+
+//actual controllers
+exports.createCategory = (req, res) => {
+  const category = new Category(req.body);
+  category.save((error, category) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Coudnot add category to DB",
+      });
+    }
+    res.status(200).json(category);
+  });
+};
+exports.getCategory = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.getAllCategories = (req, res) => {
+  Category.find().exec((error, categories) => {
+    if (error) {
+      return res.status(400).json({
+        error: "No categories found",
+      });
+    }
+    res.json(categories);
+  });
+};
+
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((error, updatedCategory) => {
+    if (error) {
+      res.status(400).json({
+        error: "Failed to update category",
+      });
+    }
+    res.json(updatedCategory);
+  });
+};
+
+exports.removeCategory = (req, res) => {
+  const category = req.category;
+  category.remove((error, category) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Failed to delete category",
+      });
+    }
+    res.json({
+      message: req.category.name + "Successfully deleted",
+    });
+  });
+};
--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getCategoryById } = require("../controllers/category");
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategories,
+  updateCategory,
+  removeCategory,
+} = require("../controllers/category");
 const { isAuthenticated, isSignedIn, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -9,5 +16,36 @@ router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
 
 //actual routes
+
+//create routes
+router.post(
+  "/category/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
+
+//read routes
+router.get("/category/:categoryId", getCategory);
+router.get("/category/categories", getAllCategories);
+
+//update route
+router.put(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateCategory
+);
+
+//delete route
+router.delete(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  removeCategory
+);
 
 module.exports = router;
