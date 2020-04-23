59b87978ad3dac869bd71b8d064e7b5523e37cb1
Mayur <mayur.jaganmohan@gmail.com>
2020-04-23 23:24:28 +0530

    Issue in getallcategories

--- controllers/category.js
+++ controllers/category.js
@@ -32,6 +32,7 @@ exports.getCategory = (req, res) => {
 exports.getAllCategories = (req, res) => {
   Category.find().exec((error, categories) => {
     if (error) {
+      console.log("Error : " + error);
       return res.status(400).json({
         error: "No categories found",
       });
