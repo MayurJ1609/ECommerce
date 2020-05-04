4d9452af03f74ac296b2ad800cd69d9d20e852f6
Mayur <mayur.jaganmohan@gmail.com>
2020-05-04 20:59:04 +0530

    Backend issue fix

--- controllers/paymentB.js
+++ controllers/paymentB.js
@@ -1,42 +1,43 @@
-var braintree = require("braintree");
+var braintree = require('braintree');
 
 var gateway = braintree.connect({
-  environment: braintree.Environment.Sandbox,
-  merchantId: "yx8kffpb4xw9hfk2",
-  publicKey: "bydthjpv9bkmh86b",
-  privateKey: "01078dd3c04a9a0debd76cdd83835327",
+	environment: braintree.Environment.Sandbox,
+	merchantId: 'yx8kffpb4xw9hfk2',
+	publicKey: 'bydthjpv9bkmh86b',
+	privateKey: '01078dd3c04a9a0debd76cdd83835327',
 });
 
 exports.getToken = (req, res) => {
-  gateway.clientToken.generate({}, function (err, response) {
-    if (err) {
-      console.log(err);
+	gateway.clientToken.generate({}, function (err, response) {
+		if (err) {
+			console.log(err);
 
-      res.status(400).send(err);
-    } else {
-      console.log(response);
-      res.send(response);
-    }
-  });
+			res.status(400).send(err);
+		} else {
+			console.log(response);
+			res.send(response);
+		}
+	});
 };
 
 exports.processPayment = (req, res) => {
-  let nonceFromTheClient = req.body.paymentMethodNonce;
-  let amountFromClient = req.body.amount;
-  gateway.transaction.sale(
-    {
-      amount: amountFromClient,
-      paymentMethodNonce: nonceFromTheClient,
-      options: {
-        submitForSettlement: true,
-      },
-    },
-    function (err, result) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send(response);
-      }
-    }
-  );
+	let nonceFromTheClient = req.body.paymentMethodNonce;
+	let amountFromClient = req.body.amount;
+	gateway.transaction.sale(
+		{
+			amount: amountFromClient,
+			paymentMethodNonce: nonceFromTheClient,
+			options: {
+				submitForSettlement: true,
+			},
+		},
+		function (err, result) {
+			if (err) {
+				res.status(500).send(err);
+			} else {
+				console.log('Payment successful');
+				res.send(result);
+			}
+		}
+	);
 };
